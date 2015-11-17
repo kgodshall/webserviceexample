ee3e505eefa6cfd9b02316d579edff7b53ddafd6
Kira Godshall <kgodshall@sch.org>
2015-11-17 11:42:06 -0500

    Initial Commit

---
+++ Web Service Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Web Service Example
+//
+//  Created by Kira Godshall on 11/17/15.
+//  Copyright © 2015 Kira Godshall. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
